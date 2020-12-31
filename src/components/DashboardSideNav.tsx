1522463540ca66fad241a998e110aea5aab04c8c
Burhan <burhan221b@gmail.com>
2020-12-31 11:07:18 -0800

    NavSideBar GUI Updates

--- src/components/DashboardSideNav.tsx
+++ src/components/DashboardSideNav.tsx
@@ -27,16 +27,16 @@ const DashboardSideNav: React.FunctionComponent<DashboardSideNavProps> = () => {
             <DashboardProfile />
             {/* Links To Different Pages/Topics */}
             <ul className="dashboard-nav-side-main-topics">
-                <li className={location.pathname === "/" ? "active" : ""}><Link onClick={removeDashboardActive} to="/" ><span className="nav-html-icons">&#9881;</span> Dashboard</Link></li>
-                <li className={location.pathname === "/devices" ? "active" : ""}><Link onClick={removeDashboardActive} to="/devices" ><span className="nav-html-icons">&#9881;</span> Devices</Link></li>
-                <li className={location.pathname === "/history" ? "active" : ""}><Link onClick={removeDashboardActive} to="/history" ><span className="nav-html-icons">&#9881;</span> History</Link></li>
-                <li className={location.pathname === "/settings" ? "active" : ""}><Link onClick={removeDashboardActive} to="/settings" ><span className="nav-html-icons">&#9881;</span> Settings</Link></li>
-                <li className={location.pathname === "/orders" ? "active" : ""}><Link onClick={removeDashboardActive} to="/orders" ><span className="nav-html-icons">&#9881;</span> Orders</Link></li>
-                <li className={location.pathname === "/help" ? "active" : ""}><Link onClick={removeDashboardActive} to="/help" ><span className="nav-html-icons">&#9881;</span> Help</Link></li>
-                <li className={location.pathname === "/aboutus" ? "active" : ""}><Link onClick={removeDashboardActive} to="/aboutus" ><span className="nav-html-icons">&#9881;</span> About Us</Link></li>
+                <Link onClick={removeDashboardActive} to="/" ><li className={location.pathname === "/" ? "active" : ""}><span className="nav-html-icons">&#9881;</span> Dashboard</li></Link>
+                <Link onClick={removeDashboardActive} to="/devices" ><li className={location.pathname === "/devices" ? "active" : ""}><span className="nav-html-icons">&#9881;</span> Devices</li></Link>
+                <Link onClick={removeDashboardActive} to="/history" ><li className={location.pathname === "/history" ? "active" : ""}><span className="nav-html-icons">&#9881;</span> History</li></Link>
+                <Link onClick={removeDashboardActive} to="/settings" ><li className={location.pathname === "/settings" ? "active" : ""}><span className="nav-html-icons">&#9881;</span> Settings</li></Link>
+                <Link onClick={removeDashboardActive} to="/orders" ><li className={location.pathname === "/orders" ? "active" : ""}><span className="nav-html-icons">&#9881;</span> Orders</li></Link>
+                <Link onClick={removeDashboardActive} to="/help" ><li className={location.pathname === "/help" ? "active" : ""}><span className="nav-html-icons">&#9881;</span> Help</li></Link>
+                <Link onClick={removeDashboardActive} to="/aboutus" ><li className={location.pathname === "/aboutus" ? "active" : ""}><span className="nav-html-icons">&#9881;</span> About Us</li></Link>
+                {/* Version Number */}
+                <Version />
             </ul>
-            {/* Version Number */}
-            <Version />
         </nav>
     );
 }
